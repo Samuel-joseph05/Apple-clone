a430d5f24da5b047ca521d82a4111800261fea2f
Samuel-joseph05 <samuela05022004@gmail.com>
2025-08-26 21:41:41 +0530

    chore: create Home component for displaying accessories

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div>
+      <div className="navbar">
+<img src="/apple-logo.svg" alt="apple icon"class="w-5 h-5" />
+        <p></p>
+
+      </div>
+      <div className="main">
+        <h1 class="text-2xl">Accessories</h1>
+      </div>
+    </div>
+  );
+}
+export default Home;
