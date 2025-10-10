8856679b279fe5f565a10d1710b62ea149f1a8b2
Samuel-joseph05 <samuela05022004@gmail.com>
2025-10-10 12:00:04 +0530

    chore: refactor App component to include routing and slides for Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-
-import './App.css'
-import Home from './pages/Home.jsx'
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Store from "./components/Store.jsx";
+import SearchBar from "./pages/SearchBar.jsx";
 function App() {
-  
+  let slides = {
+    // <img src="img_1.jpg" className="w-70 h-50"></img>
+    //  [<img src="img_1.jpg" className="w-70 h-50"></img>] ,
+    //   <img src="img_1.jpg" className="w-70 h-50"></img>,
+    //   <img src="img_1.jpg" className="w-70 h-50"></img>
+  };
   return (
     <>
-<Home/>
+      <Router>
+        <Routes>
+          <Route path="/" element={ <Home slides={slides} />}></Route>
+          <Route path="/store" element={ <Store />}></Route>
+          <Route path="/searchbar" element={<SearchBar />}></Route>
+        </Routes>
+      </Router>
+     
+     
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
