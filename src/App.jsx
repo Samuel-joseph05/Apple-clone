2ff0bbdd9456b83286bc7e8a0f2fd2aa71dc3304
Samuel-joseph05 <samuela05022004@gmail.com>
2025-10-19 01:05:24 +0530

    feat: add Menu route to App component for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Store from "./components/Store.jsx";
 import SearchBar from "./pages/SearchBar.jsx";
+import Menu from "./pages/Menu.jsx";
 
 function App() {
- 
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>}></Route>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/store" element={<Store />}></Route>
           <Route path="/searchbar" element={<SearchBar />}></Route>
+          <Route path="/menubar" element={<Menu />}></Route>
         </Routes>
       </Router>
-  
     </>
   );
 }
