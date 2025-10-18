9a800569493fce81838cfa757f1edbe7cd50db20
Samuel-joseph05 <samuela05022004@gmail.com>
2025-10-18 21:10:57 +0530

    chore: clean up App component by removing unused slides variable and comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,19 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Store from "./components/Store.jsx";
 import SearchBar from "./pages/SearchBar.jsx";
+
 function App() {
-  let slides = {
-    // <img src="img_1.jpg" className="w-70 h-50"></img>
-    //  [<img src="img_1.jpg" className="w-70 h-50"></img>] ,
-    //   <img src="img_1.jpg" className="w-70 h-50"></img>,
-    //   <img src="img_1.jpg" className="w-70 h-50"></img>
-  };
+ 
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={ <Home slides={slides} />}></Route>
-          <Route path="/store" element={ <Store />}></Route>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/store" element={<Store />}></Route>
           <Route path="/searchbar" element={<SearchBar />}></Route>
         </Routes>
       </Router>
-     
-     
-
+  
     </>
   );
 }
